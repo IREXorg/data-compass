760341644a410fa797ab62202ae3b0e81b357c10
Joshua Mabina <mabinajoshua@gmail.com>
2020-02-10 12:24:13 +0300

    feature(questionnaires): add questionnaires app
    Added questionnaires implementation for admin.

--- apps/questionnaires/admin.py
+++ apps/questionnaires/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Questionnaire
+
+
+@admin.register(Questionnaire)
+class QuestionnaireAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'uuid', 'name', 'phase']
+    readonly_fields = ['id', 'uuid', 'created_at', 'modified_at']
+    list_display = ['name', 'description', 'phase']
+    list_filter = ['phase']
+    autocomplete_fields = ['project', 'organization']
+
+    def save_model(self, request, obj, form, change):
+        if not change or not obj.creator:
+            obj.creator = request.user
+        super().save_model(request, obj, form, change)
