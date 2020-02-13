56dffe8d750954675b56163ca46b6def233e4e42
Joshua Mabina <mabinajoshua@gmail.com>
2020-02-13 16:17:45 +0300

    feat(frontend-scaffolding): added build script
    added build script for compiling to production.

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: './assets/fonts/'
+              outputPath: './fonts/'
             }
           }
         ]
