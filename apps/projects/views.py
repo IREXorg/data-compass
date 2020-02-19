5afa00bd2ade4555e57018dfe1dc3514edc3001a
Joshua Mabina <mabinajoshua@gmail.com>
2020-02-19 12:35:21 +0300

    feature(manage-projects): stubbed project list

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from apps.projects.models import Project
 
 def index(request):
-    context = {}
+    p = Project(name='Foo Name', description='This is a description')
+    projects = [p,[]]
+
+    context = { 'projects': projects }
 
     return render(request, 'projects/index.html', context)
