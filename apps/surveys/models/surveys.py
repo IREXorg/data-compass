28a091ff76d45df3cb59ec7ee47734afb8d632c6
machakux <bmachaku@gmail.com>
2020-02-21 12:08:46 +0300

    fix merge conflicts

--- apps/surveys/models/surveys.py
+++ apps/surveys/models/surveys.py
@@ -1,17 +1,13 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.postgres.fields import ArrayField, JSONField
-from django.core.exceptions import PermissionDenied
+from django.contrib.postgres.fields import JSONField
 from django.db import models
-from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 
 from mptt.models import MPTTModel, TreeForeignKey
 
 from core.models import TimeStampedModel
-
-from ..managers import SurveyManager
 
 
 class DataflowHierarchy(TimeStampedModel, MPTTModel):
@@ -23,7 +19,6 @@ class DataflowHierarchy(TimeStampedModel, MPTTModel):
     )
     project = models.ForeignKey(
         'projects.Project',
-        blank=True,
         verbose_name=_('project'),
         on_delete=models.CASCADE,
         related_name='hierarchies',
@@ -37,7 +32,7 @@ class DataflowHierarchy(TimeStampedModel, MPTTModel):
         related_name='children',
         verbose_name=_('parent')
     )
-    level_name = models.CharField(_('level name'), max_length=128)
+    level_name = models.CharField(_('level name'), max_length=128, unique=True)
     name = models.CharField(_('name'), max_length=128, unique=True)
     description = models.TextField(_('description'), blank=True)
     creator = models.ForeignKey(
@@ -58,11 +53,6 @@ class DataflowHierarchy(TimeStampedModel, MPTTModel):
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        if self.parent:
-            self.project = self.parent.project
-        super().save(*args, **kwargs)
 
 
 class Role(TimeStampedModel):
@@ -97,156 +87,6 @@ class Role(TimeStampedModel):
 
     def __str__(self):
         return self.name
-
-
-class Survey(TimeStampedModel):
-    """A Survey model
-
-    If the survey has :attr:`~login_required` set to ``True`` only logged in
-    users should be allowed to respond to the survey.
-
-    If the survey has :attr:`~invitation_required` set to ``True`` only
-    users only invited users should be allowed to respond to the survey.
-    Currently this only registered users can be invitees by being pre-added
-    to the respondent list but this will change in future.
-    """
-    uuid = models.UUIDField(
-        _('UUID'),
-        default=uuid.uuid4,
-        editable=False,
-        unique=True
-    )
-    project = models.ForeignKey(
-        'projects.project',
-        related_name='surveys',
-        related_query_name='survey',
-        verbose_name=_('project'),
-        on_delete=models.CASCADE
-    )
-    name = models.CharField(_('name'), max_length=255)
-    description = models.TextField(_('description'), blank=True)
-    display_name = models.CharField(
-        _('display name'),
-        max_length=255,
-        blank=True
-    )
-    research_question = models.CharField(_('research question'), max_length=255)
-    languages = ArrayField(
-        models.CharField(max_length=5),
-        verbose_name=_('languages'),
-        blank=True,
-        default=list
-    )
-    code = models.SlugField(
-        _('code'),
-        blank=True,
-        allow_unicode=True,
-        unique=True
-    )
-    login_required = models.BooleanField(
-        _('login required'),
-        help_text=_('Do you want users to login before responding to the survey?'),
-        blank=True,
-        default=True
-    )
-    invitation_required = models.BooleanField(
-        _('invitation required'),
-        help_text=_('Do you want the survey to be taken by invited users only?'),
-        blank=True,
-        default=True
-    )
-    respondent_can_aggregate = models.BooleanField(
-        _('respondent can aggregate'),
-        help_text=_("Do you want repondents to see visualizations or aggregates of other users' responses?"),
-        blank=True,
-        default=True
-    )
-    respondent_can_invite = models.BooleanField(
-        _('respondent can suggest others'),
-        help_text=_('Do you want users to share email addresses of other potential respondents?'),
-        blank=True,
-        default=True
-    )
-    is_active = models.BooleanField(
-        _('is active'),
-        help_text=_('Is published'),
-        blank=True,
-        default=True
-    )
-    creator = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        verbose_name=_('creator'),
-        blank=True,
-        related_name='created_surveys',
-        related_query_name='created_survey',
-        on_delete=models.CASCADE
-    )
-    extras = JSONField(_('extras'), blank=True, default=dict)
-
-    #: Default manager.
-    objects = SurveyManager()
-
-    class Meta:
-        verbose_name = _('Survey')
-        verbose_name_plural = _('Surveys')
-
-    def __str__(self):
-        return self.display_name
-
-    def save(self, *args, **kwargs):
-        if not self.code:
-            self.code = slugify(self.name[:50])
-        if not self.display_name:
-            self.display_name = self.name
-        super().save(*args, **kwargs)
-
-    def get_or_create_respondent(self, user=None, email=None):
-        """
-        Get or create respondent.
-
-        Returns: (respondent, created). If a new respondent was created in
-        the process the second value of the tuple (created) will be ``True``.
-        """
-        # if login/user is not required
-        if not self.login_required:
-            if user and user.is_authenticated:
-                return self.respondents.get_or_create(user=user, survey=self)
-            elif email:
-                return self.respondents.get_or_create(email=email, survey=self)
-
-            respondent = self.respondents.create(survey=self)
-            return (respondent, True)
-
-        # user is required
-        if not user.is_authenticated:
-            raise PermissionDenied(_('A user is required'))
-        return self.respondents.get_or_create(user=user, survey=self)
-
-    def available_for_respondent(self, respondent):
-        """
-        Checks if the survey is available for respondent.
-
-        Args:
-            respondent: :class:`.Respondent` object
-
-        Returns:
-            bool: True if survey can be taken by respondent otherwise False.
-        """
-        user = None
-        if respondent:
-            user = respondent.user
-
-        if user is None and (self.login_required or self.invitation_required):
-            return False
-
-        if self.invitation_required and not self.respondents.filter(user=user).exists():
-            return False
-
-        # Make sure user is not AnonymousUser
-        if user.is_authenticated:
-            return True
-
-        return False
 
 
 class Topic(TimeStampedModel):
@@ -290,19 +130,12 @@ class Dataset(TimeStampedModel):
         editable=False,
         unique=True
     )
-    survey = models.ForeignKey(
-        'survey',
-        related_name='datasets',
-        related_query_name='dataset',
-        verbose_name=_('survey'),
-        on_delete=models.CASCADE
-    )
-    topics = models.ManyToManyField(
+    topic = models.ForeignKey(
         'surveys.Topic',
         related_name='datasets',
         related_query_name='dataset',
-        verbose_name=_('topics'),
-        blank=True
+        verbose_name=_('topic'),
+        on_delete=models.CASCADE
     )
     name = models.CharField(_('name'), max_length=255)
     description = models.TextField(_('description'), blank=True)
