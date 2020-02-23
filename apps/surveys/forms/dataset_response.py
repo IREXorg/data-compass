e1b8b7fbc1263210c1fcbed5a8ce2b83b8e56c1c
machakux <bmachaku@gmail.com>
2020-02-23 16:12:31 +0300

    feature(responses): add dataset-topic storage form

--- apps/surveys/forms/dataset_response.py
+++ apps/surveys/forms/dataset_response.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from ..models import DatasetResponse
+from ..models import DatasetResponse, DatasetTopicResponse, DatasetTopicStorageAccess, Role
 
 
 class DatasetSelectForm(forms.Form):
@@ -57,5 +57,119 @@ class DatasetResponseFrequencyForm(DatasetResponseForm):
         if self.instance:
             self.fields['dataset_frequency'].label = _(
                 'How often do you produce, access or share information '
-                'about %(dataset_name)s?'
-            ) % {'dataset_name': self.instance.dataset.name}
+                'about %(dataset)s?'
+            ) % {'dataset': self.instance.dataset.name}
+
+
+class DatasetTopicResponseForm(forms.ModelForm):
+
+    percieved_owner = forms.ModelChoiceField(
+        queryset=None,
+        widget=forms.RadioSelect,
+        required=True,
+        empty_label=None
+    )
+
+    class Meta:
+        model = DatasetTopicResponse
+        fields = ['percieved_owner']
+
+    def __init__(self, project=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            self.fields['percieved_owner'].label = _(
+                'Who at %(hierarchy)s do you believe knows most '
+                'about %(topic)s regarding %(dataset)s?'
+            ) % {
+                'dataset': self.instance.dataset_response.dataset.name,
+                'hierarchy': self.instance.dataset_response.respondent.hierarchy,
+                'topic': self.instance.topic.name
+            }
+
+        if not project and self.instance:
+            project = self.instance.dataset_response.response.survey.project
+
+        self.fields['percieved_owner'].queryset = Role.objects.filter(hierarchy__project=project)
+
+
+class DatasetTopicStorageAccessForm(forms.ModelForm):
+    selected = forms.BooleanField(required=False)
+
+    class Meta:
+        model = DatasetTopicStorageAccess
+        fields = ['selected', 'storage', 'access']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.storage_name = None
+
+        if self.initial:
+            storage = self.initial.get('storage')
+            if storage:
+                self.storage_name = storage.name
+
+        self.fields['access'].required = False
+
+    def clean(self):
+        super().clean()
+
+        if self.cleaned_data['selected']:
+            if not self.cleaned_data['access']:
+                raise forms.ValidationError(_('Please please specify how this is accessible'))
+
+
+class BaseDatasetTopicStorageAccessFormSet(forms.BaseModelFormSet):
+
+    def __init__(self, instance=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queryset = None
+        self.can_delete = False
+        self.instance = instance
+
+        if self.instance:
+            self.queryset = self.instance.storages.all()
+
+            storages = self.instance.dataset_response.response.survey.dataset_storages.all()
+
+            # set number of forms equal to storage options
+            self.extra = self.max_num = len(storages)
+
+            self.initial = [{'storage': storage} for storage in storages]
+
+            initial_selected = self.instance.storages.select_related('access', 'storage')
+            initial_storages = [item.storage for item in initial_selected]
+
+            for item in self.initial:
+                try:
+                    i = initial_storages.index(item['storage'])
+                    item['selected'] = True
+                    item['access'] = initial_selected[i].access
+                except ValueError:
+                    item['access'] = None
+                    pass
+
+    def save(self):
+        if not self.instance:
+            raise ValueError(_('Instance not specified'))
+
+        saved_forms = []
+
+        # clear existing records
+        self.instance.storages.all().delete()
+
+        # save each of the selected records
+        for form in self.forms:
+            if form.cleaned_data.get('selected'):
+                form.instance.response = self.instance
+                saved_forms.append(form.save())
+
+        return saved_forms
+
+
+DatasetTopicStorageAccessFormSet = forms.modelformset_factory(
+    DatasetTopicStorageAccess,
+    form=DatasetTopicStorageAccessForm,
+    formset=BaseDatasetTopicStorageAccessFormSet
+)
--- core/mixins.py
+++ core/mixins.py
@@ -1,3 +1,6 @@
+from django.shortcuts import redirect
+
+
 class PageTitleMixin:
     """
     Passes page_title and active_tab into context data.
@@ -32,9 +35,45 @@ class PageMixin(PageTitleMixin):
 
     def get_back_url_path(self):
         """Override this for dynamic back_path."""
-        return self.back_url_path or self.request.META['HTTP_REFERER']
+        return self.back_url_path or self.request.META.get('HTTP_REFERER')
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx.setdefault('back_url_path', self.get_back_url_path())
         return ctx
+
+
+class InlineFormsetMixin:
+    """
+    Inline Formset mixin for class based views.
+    """
+
+    def get_formset(self, form_class=None):
+        """Construct and return a formset instance."""
+        if form_class is None:
+            form_class = self.formset_class
+        return form_class(**self.get_formset_kwargs())
+
+    def get_formset_kwargs(self):
+        """Returns argumnets to be used for instatiating the formset."""
+        kwargs = {}
+
+        if self.object:
+            kwargs['instance'] = self.object
+
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def form_valid(self, form, formset):
+        """save forms and redirect to success URL"""
+        self.object = form.save()
+        formset.save()
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form, formset):
+        """If the form is invalid, render the invalid form."""
+        return self.render_to_response(self.get_context_data(form=form, formset=formset))
