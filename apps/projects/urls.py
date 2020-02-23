3a11c76c3e6125e8c9c819c06701ecec57c62f9a
machakux <bmachaku@gmail.com>
2020-02-23 17:18:39 +0300

    refactor(projects): use new django urls path

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'projects'
 
 urlpatterns = [
-    url(r'^$', views.ProjectListView.as_view(), name='project-list'),
-    url(r'^create/$', views.ProjectCreateView.as_view(), name='project-create'),
-    url(r'^(?P<pk>\d+)/$', views.ProjectDetailView.as_view(), name='project-detail'),
-    url(r'^(?P<pk>\d+)/update/$', views.ProjectUpdateView.as_view(), name='project-update'),
-    url(r'^(?P<pk>\d+)/delete/$', views.ProjectDeleteView.as_view(), name='project-delete'),
+    path('', views.ProjectListView.as_view(), name='project-list'),
+    path('create/', views.ProjectCreateView.as_view(), name='project-create'),
+    path('<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
+    path('<int:pk>/update/', views.ProjectUpdateView.as_view(), name='project-update'),
+    path('<int:pk>/delete/', views.ProjectDeleteView.as_view(), name='project-delete'),
 ]
