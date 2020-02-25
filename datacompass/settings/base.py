5680f593258faf3169bf90e771c7aeaec3294fb2
machakux <bmachaku@gmail.com>
2020-02-26 02:05:03 +0300

    feature(devops): configure logging

--- datacompass/settings/base.py
+++ datacompass/settings/base.py
@@ -251,7 +251,7 @@ ADMINS = getaddresses([env('ADMINS', default='')])
 AZURE_ACCOUNT_NAME = env('AZURE_ACCOUNT_NAME', default=None)
 
 AZURE_ACCOUNT_KEY = env('AZURE_ACCOUNT_KEY', default=None)
-''
+
 AZURE_CONTAINER = env('AZURE_CONTAINER', default=None)
 
 AZURE_STATIC_CONTAINER = env('AZURE_STATIC_CONTAINER', default=None)
@@ -267,3 +267,58 @@ AZURE_STATIC_LOCATION = env('AZURE_STATIC_LOCATION', default='static')
 AZURE_MEDIA_LOCATION = env('AZURE_MEDIA_LOCATION', default='media')
 
 AZURE_CUSTOM_DOMAIN = env('AZURE_CUSTOM_DOMAIN', default=None)
+
+
+# Logging
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
+		},
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
+		},
+	},
+	'formatters': {
+		'django.server': {
+			'()': 'django.utils.log.ServerFormatter',
+			'format': '[%(server_time)s] %(message)s',
+		}
+	},
+	'handlers': {
+		'console': {
+			'level': 'INFO',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'console_debug_false': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'logging.StreamHandler',
+		},
+		'django.server': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+			'formatter': 'django.server',
+		},
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console', 'console_debug_false', 'mail_admins'],
+			'level': 'INFO',
+		},
+		'django.server': {
+			'handlers': ['django.server'],
+			'level': 'INFO',
+			'propagate': False,
+		}
+	}
+}
