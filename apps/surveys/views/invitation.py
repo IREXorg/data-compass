6ae26f7e3c7c44ef69de2de8f6e3e22886ee9c15
Joshua Mabina <mabinajoshua@gmail.com>
2020-03-11 22:17:15 +0300

    feat(invitations): list available invitations
    list available invitations to send invitations to.
    
    see #86

--- apps/surveys/views/invitation.py
+++ apps/surveys/views/invitation.py
@@ -1,27 +1,25 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy as reverse
 from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import redirect
-
-from apps.projects.models import Project
-from core.mixins import PageTitleMixin
 
 from invitations.utils import get_invitation_model
 from invitations.views import SendInvite
 
+from apps.projects.models import Project
+from core.mixins import PageMixin
+from django.views.generic.list import ListView
+
 Invitation = get_invitation_model()
 
-class SendInviteView(SendInvite):
-    template_name='invitations/send_invite.html'
-    page_title='Send Invite'
+class SendInviteView(PageMixin, SendInvite, ListView):
+    template_name = 'invitations/send_invite.html'
+    page_title = 'Send Invite'
+    context_object_name = 'respondents'
+    queryset = Invitation.objects.all()
 
     def form_valid(self, form):
-        print(f'-----{self.request}-----')
-        print(f'-----{form.cleaned_data}-----')
-
         invite = Invitation.create(form.cleaned_data['email'], inviter=self.request.user)
         invite.send_invitation(self.request)
 
         return redirect('/invitations/send-invite')
-
-
