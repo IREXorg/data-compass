55edeb899c2656f33d96daada825d10725bec998
lykmapipo <lallyelias87@gmail.com>
2020-03-11 08:38:14 +0300

    feat(survey): add logo model

--- apps/surveys/models/surveys.py
+++ apps/surveys/models/surveys.py
@@ -318,3 +318,42 @@ class Choice(TimeStampedModel):
 
     def __str__(self):
         return self.label
+
+
+class Logo(TimeStampedModel):
+    uuid = models.UUIDField(
+        _('UUID'),
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    survey = models.ForeignKey(
+        'survey',
+        related_name='logos',
+        related_query_name='logo',
+        verbose_name=_('survey'),
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(_('name'), max_length=255)
+    logo = models.ImageField(
+        _('logo'),
+        blank=True,
+        null=True,
+        upload_to='surveys/logos'
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('creator'),
+        blank=True,
+        related_name='created_survey_logos',
+        related_query_name='created_survey_logo',
+        on_delete=models.CASCADE
+    )
+    extras = JSONField(_('extras'), blank=True, default=dict)
+
+    class Meta:
+        verbose_name = _('Logo')
+        verbose_name_plural = _('Logos')
+
+    def __str__(self):
+        return self.name
