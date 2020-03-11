c4068b0cef70e0b42724abf74af06cb52a77cfa2
machakux <bmachaku@gmail.com>
2020-03-11 19:49:30 +0300

    optimize: add indexes

--- core/models.py
+++ core/models.py
@@ -7,8 +7,16 @@ class TimeStampedModel(models.Model):
     An abstract base class model that provides self-updating
     ``created_at`` and ``modified_at`` fields.
     """
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
-    modified_at = models.DateTimeField(_('modified at'), auto_now=True)
+    created_at = models.DateTimeField(
+        _('created at'),
+        auto_now_add=True,
+        db_index=True
+    )
+    modified_at = models.DateTimeField(
+        _('modified at'),
+        auto_now=True,
+        db_index=True
+    )
 
     class Meta:
         abstract = True
