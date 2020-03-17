c06c1ec1bc08f1bfd91f0d031878eac4270be5d6
lykmapipo <lallyelias87@gmail.com>
2020-03-17 09:08:23 +0300

    refactor(surveys): rename respondent and question views & forms

--- apps/surveys/forms/__init__.py
+++ apps/surveys/forms/__init__.py
@@ -2,8 +2,8 @@ from .dataset import *  # noqa
 from .dataset_storage import *  # noqa
 from .entity import *  # noqa
 from .logo import *  # noqa
+from .question import *  # noqa
+from .respondent import *  # noqa
 from .role import *  # noqa
 from .survey import *  # noqa
-from .survey_question import *  # noqa
-from .survey_respondent import *  # noqa
 from .topic import *  # noqa
